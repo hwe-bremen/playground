09ca266dbce9b48f12df2fe5e858b69b07a1c23f
hwe-bremen <43620549+hwe-bremen@users.noreply.github.com>
2020-11-05 15:12:32 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,5 @@ function bulliFahr() {
     var div_bulli = document.getElementById("div_bulli"); 
     div_bulli.classList.toggle("bullilos");
     }
-var buttoneins = document.getElementById('button-click');
- 	buttoneins.addEventListener('click', 'bulliFahr');
+var buttoneins = document.getElementById("button-click");
+ 	buttoneins.addEventListener('click', "bulliFahr");
