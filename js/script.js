db43f2c36fcf91b17083a42bd03e773e5dc55e46
hwe-bremen <43620549+hwe-bremen@users.noreply.github.com>
2020-11-05 13:49:52 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,15 +22,33 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-function elementSichtbar() {   
-    var boxVier = document.getElementById("box4"); 
-    boxVier.classList.toggle("sichtbar");
+function elementSichtbar() {  document.addEventListener('DOMContentLoaded', function () {
+	document.querySelector('#interaktiv')
+		.addEventListener('click', animiere);
+
+	function animiere() {
+		var ladebalken = document.getElementById('ladebalken');
+		ladebalken.animate(
+      [
+				{
+					width: '1em',
+					background: 'green'
+        }, {
+					width: '20em',
+					background: 'lime'
+        }
+      ], {
+				duration: 2000,
+				iterations: 1,
+				fill: 'forwards'
+			});
+	}
+});
+
+
+function bulliFahr() {   
+    var div_bulli = document.getElementById("div_bulli"); 
+    div_bulli.classList.toggle("bullilos");
     }
-   function elementSichtbar2() { 
-	var buttonClick = document.getElementById("button-click"); 
-    buttonClick.classList.toggle("buttonstylehover");
-     }
-	
-	var buttonzwei = document.getElementById('button-click')
- 	buttonzwei.addEventListener('click', elementSichtbar);
- 	buttonzwei.addEventListener('click', elementSichtbar2);
+var buttoneins = document.getElementById('button-click')
+ 	buttoneins.addEventListener('click', bulliFahr);
